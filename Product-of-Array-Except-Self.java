82cfdb0bb2edb63dd46f761e4e41b91bc3a96677
eburkk <48260610+eburkk@users.noreply.github.com>
2019-11-07 12:34:50 -0800

    Update Product-of-Array-Except-Self.java

--- Product-of-Array-Except-Self.java
+++ Product-of-Array-Except-Self.java
@@ -57,5 +57,29 @@ class Solution {
     }
 }
 
+// No division with O(n) runtime 
+class Solution {
+    public int[] productExceptSelf(int[] nums) {
+        int size = nums.length;         // length of input array
+        int[] output = new int[size];   // final output array
+        int[] left = new int[size];     // product of numbers to left
+        int[] right = new int[size];    //product of numbers to right
+        left[0] = 1;
+        right[size - 1] = 1;
+        
+        for (int i = 1; i < size; i++) {
+            left[i] = left[i - 1] * nums[i - 1];    
+        }
+        
+        for (int i = size - 2; i >= 0 ; i--) {
+            right[i] = right[i + 1] * nums[i + 1];
+        }
+        
+        for (int i = 0; i < size; i++) {
+            output[i] = left[i] * right[i];
+        }
+        return output;
+    }
+}
 
 
