fe2dc03c48f1ee33dd773a04462bc5b765f8adf8
eburkk <48260610+eburkk@users.noreply.github.com>
2019-11-07 12:19:13 -0800

    Update Product-of-Array-Except-Self.java

--- Product-of-Array-Except-Self.java
+++ Product-of-Array-Except-Self.java
@@ -1,10 +1,15 @@
 // Problem: 
-/* Given an array nums of n integers, return an array output such that output[i] is equal to the product of all the elements of nums except nums[i]. */
+/* Given an array nums of n integers, return an array output such that output[i] is equal to
+    the product of all the elements of nums except nums[i]. */
 
 // Questions: 
-//      Can integers be negative numbers?
+//      Can integers be negative numbers? --> yes
 //      What should I return if array is empty? or length 1?--> guaranteed nums.length > 1
-//      Are the n integer values unique? Can there be duplicates? 
+//      Are the n integer values unique? Can there be duplicates? --> not unique values
+
+// Edge Cases:
+// All zero [0, 0]
+// All but 1 zero, [0, 4, 0]
 
 // Brute force solution:
 //      Nested for loop. O(n^2) time complexity
@@ -23,3 +28,34 @@ class Solution {
         return output;
     }
 }
+
+// O(n) runtime using division
+class Solution {
+    public int[] productExceptSelf(int[] nums) {
+        int[] output = new int[nums.length];
+        int product = 1;
+        int zeroCount = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != 0) {
+                product *= nums[i];
+            } else {
+                zeroCount++;
+            }
+        }
+        
+        for (int i = 0; i < nums.length; i ++) {
+            if (nums[i] != 0 && zeroCount == 0) {
+                output[i] = product / nums[i];
+            } else if (nums[i] == 0 && zeroCount == 1) {
+                output[i] = product;
+            } else if (zeroCount > 1) {
+                output[i] = 0;
+            }
+        }
+        return output;
+    }
+}
+
+
+
