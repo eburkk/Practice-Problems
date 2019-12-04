c2aacd36cd5e1b50e71a8b737287fdcb979c62cb
Erika Burk <48260610+eburkk@users.noreply.github.com>
2019-12-03 17:44:43 -0800

    Update Two-Sum.java

--- Two-Sum.java
+++ Two-Sum.java
@@ -7,8 +7,31 @@
 //  make sure not to count the same indice as two numbers
 // empty array or array length < 2
 
+// use hash map for constant time look up. 
+class Solution2 {
+    public int[] twoSum(int[] nums, int target) {
+        int[] solution = new int[2];
+        if (nums == null || nums.length < 2) {
+            return solution;
+        }
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int j = 0; j < nums.length; j++) {
+            map.put(nums[j], j);
+        }
+        for (int i = 0; i < nums.length; i++) {
+            int dif = target - nums[i];
+            if (map.containsKey(dif) && map.get(dif) != i) {
+                solution[0] = i;
+                solution[1] = map.get(dif);
+                return solution;
+            }
+        }
+        return solution;
+    }
+}
+
 // Brute Force Solution 
-class Solution {
+class Solution1 {
     public int[] twoSum(int[] nums, int target) {
         int[] solution = new int[2];
         if (nums == null || nums.length < 2) {
